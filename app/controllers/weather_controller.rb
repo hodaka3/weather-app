afd13d0247614e7195ae07013a33e46c68417560
hodaka3 <hodaka.sato1026@gmail.com>
2026-04-23 05:07:34 +0900

    add auto fetch if data is empty

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,6 +1,10 @@
 class WeatherController < ApplicationController
-    def index
-        # 10件だけ表示
-        @weathers = WeatherRecord.order(:datetime).limit(20) 
+  def index
+    # データが1件もなければ、その場で取得を実行する
+    if WeatherRecord.count == 0
+      WeatherRecord.fetch_and_save
     end
+
+    @weathers = WeatherRecord.order(datetime: :desc).limit(20)
+  end
 end
