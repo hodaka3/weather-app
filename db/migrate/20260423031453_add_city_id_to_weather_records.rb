91261e7b2aab82211a3749b10f8aef470313b4a0
hodaka3 <hodaka.sato1026@gmail.com>
2026-04-23 13:41:48 +0900

    fix city_id null violation in migration

--- db/migrate/20260423031453_add_city_id_to_weather_records.rb
+++ db/migrate/20260423031453_add_city_id_to_weather_records.rb
@@ -1,5 +1,17 @@
-class AddCityIdToWeatherRecords < ActiveRecord::Migration[8.1]
+class AddCityIdToWeatherRecords < ActiveRecord::Migration[7.0]
   def change
-    add_reference :weather_records, :city, null: false, foreign_key: true
+    # 1. まずは null: true (空OK) でカラムを追加する
+    add_reference :weather_records, :city, null: true, foreign_key: true
+
+    # 2. すでにある古いデータを削除するか、適当な都市を割り当てる
+    # (本番の古いデータが不要なら削除するのが一番早いです)
+    reversible do |dir|
+      dir.up do
+        execute "DELETE FROM weather_records" 
+      end
+    end
+
+    # 3. データが空になったので、改めて null: false 制約をつける
+    change_column_null :weather_records, :city_id, false
   end
 end
