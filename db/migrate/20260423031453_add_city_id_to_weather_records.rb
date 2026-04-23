9be9f2933fc7efaa37f6d6fc84e1a764be8d12f4
hodaka3 <hodaka.sato1026@gmail.com>
2026-04-23 13:48:16 +0900

    fix migration by truncating old weather records

--- db/migrate/20260423031453_add_city_id_to_weather_records.rb
+++ db/migrate/20260423031453_add_city_id_to_weather_records.rb
@@ -1,17 +1,18 @@
 class AddCityIdToWeatherRecords < ActiveRecord::Migration[7.0]
-  def change
-    # 1. まずは null: true (空OK) でカラムを追加する
-    add_reference :weather_records, :city, null: true, foreign_key: true
-
-    # 2. すでにある古いデータを削除するか、適当な都市を割り当てる
-    # (本番の古いデータが不要なら削除するのが一番早いです)
-    reversible do |dir|
-      dir.up do
-        execute "DELETE FROM weather_records" 
-      end
+  def up
+    # 1. もし既に中途半端にカラムが存在していたら削除（エラー回避の安全策）
+    if column_exists?(:weather_records, :city_id)
+      remove_reference :weather_records, :city, foreign_key: true
     end
 
-    # 3. データが空になったので、改めて null: false 制約をつける
-    change_column_null :weather_records, :city_id, false
+    # 2. 原因となる古い「履歴データ」をテーブルから完全に消去
+    execute "TRUNCATE TABLE weather_records RESTART IDENTITY CASCADE"
+
+    # 3. テーブルが空になったので、正しい制約でカラムを追加
+    add_reference :weather_records, :city, null: false, foreign_key: true
+  end
+
+  def down
+    remove_reference :weather_records, :city
   end
 end
